035fd04f0a57f2c7fcad5f0150dfcc5da5d202ee
handsome_man <1073806594@qq.com>
2020-10-20 15:51:52 +0800

    第二次提交

---
+++ src/components/subMenu.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="subMenu">
+    <h1 class="logo">
+      神圣学院
+    </h1>
+    <el-menu
+      default-active="1-4-1"
+      class="el-menu-vertical-demo"
+      @open="handleOpen"
+      @close="handleClose"
+      :collapse="isCollapse"
+    >
+      <!-- 常规菜单 -->
+      <el-menu-item index="1">
+        <i class="el-icon-menu"></i>
+        <span slot="title">管理首页</span>
+      </el-menu-item>
+      <!-- 具有子菜单 -->
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-s-custom"></i>
+          <span slot="title">学员管理</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="/product">项目管理</el-menu-item>
+          <el-menu-item index="1-1">选项1</el-menu-item>
+          <el-menu-item index="1-2">选项2</el-menu-item>
+          <el-menu-item index="1-3">选项3</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+      <!-- 常规菜单 -->
+      <el-menu-item index="2">
+        <i class="el-icon-menu"></i>
+        <span slot="title">考勤管理</span>
+      </el-menu-item>
+      <!-- 常规菜单 -->
+      <el-menu-item index="3">
+        <i class="el-icon-menu"></i>
+        <span slot="title">数据统计</span>
+      </el-menu-item>
+      <!-- 常规菜单 -->
+      <el-menu-item index="4">
+        <i class="el-icon-menu"></i>
+        <span slot="title">我的中心</span>
+      </el-menu-item>
+    </el-menu>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      isCollapse: false,
+    };
+  },
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    },
+  },
+};
+</script>
+<style>
+.subMenu{
+  width: 200px;
+  overflow: hidden;
+}
+.logo {
+  text-align: center;
+  height: 60px;
+  line-height: 60px;
+  color: #fff;
+  background: linear-gradient(135deg, #4c67ff, #5635df);
+  background-size: 100%;
+  background-repeat: no-repeat;
+  background-position: 50%;
+}
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+  min-height: 400px;
+}
+</style>
--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -3,69 +3,15 @@
     <el-container>
       <!-- 侧边栏 -->
       <el-aside width="200px">
-        <el-menu
-          default-active="1-4-1"
-          class="el-menu-vertical-demo"
-          @open="handleOpen"
-          @close="handleClose"
-          :collapse="isCollapse"
-        >
-          <el-menu-item index="0">
-            <i class="el-icon-menu"></i>
-            <span slot="title">管理首页</span>
-          </el-menu-item>
-
-          <el-submenu index="1">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span slot="title">学员管理</span>
-            </template>
-            <el-menu-item-group>
-              <el-menu-item index="1-1">选项1</el-menu-item>
-              <el-menu-item index="1-2">选项2</el-menu-item>
-              <el-menu-item index="1-3">选项3</el-menu-item>
-            </el-menu-item-group>
-          </el-submenu>
-
-          <el-menu-item index="2">
-            <i class="el-icon-menu"></i>
-            <span slot="title">考勤管理</span>
-          </el-menu-item>
-
-          <el-menu-item index="3">
-            <i class="el-icon-menu"></i>
-            <span slot="title">数据统计</span>
-          </el-menu-item>
-
-          <el-menu-item index="4">
-            <i class="el-icon-menu"></i>
-            <span slot="title">我的中心</span>
-          </el-menu-item>
-        </el-menu>
+        <subMenu></subMenu>
       </el-aside>
       <el-container>
         <el-header>
           <!-- 顶部栏 -->
           <el-row type="flex" class="row-bg" justify="space-between">
-            <el-col :span="1">
-              <div class="grid-content bg-purple">
-                <el-radio-group
-                  v-model="isCollapse"
-                  style="margin-bottom: 20px;"
-                >
-                  <el-radio-button
-                    v-if="isCollapse"
-                    :label="false"
-                  >
-                    <i class="el-icon-s-fold"></i>
-                  </el-radio-button>
-                  <el-radio-button
-                    v-if="!isCollapse"
-                    :label="true"
-                  >
-                    收起
-                  </el-radio-button>
-                </el-radio-group>
+            <el-col :span="6">
+              <div class="grid-content">
+                图标
               </div>
             </el-col>
             <el-col :span="6">
@@ -73,7 +19,7 @@
                 学生管理系统
               </div>
             </el-col>
-            <el-col :span="3">
+            <el-col :span="6">
               <div class="grid-content bg-purple">
                 <div id="logo-img">
                   <img src="" alt="" />
@@ -86,79 +32,15 @@
         </el-header>
         <el-main>
           <!-- 主体 -->
+          <router-view></router-view>
         </el-main>
       </el-container>
     </el-container>
   </div>
 </template>
-<style>
-#logo-img {
-  border-radius: 50%;
-  width: 35px;
-  height: 35px;
-  /* background: black; */
-  display: inline-block;
-}
-.el-menu-item-group__title {
-  padding: 0;
-}
-.el-menu-vertical-demo:not(.el-menu--collapse) {
-  width: 200px;
-  min-height: 400px;
-}
-.el-row {
-}
-.el-aside {
-  /* width: 64px !important; */
-}
-.el-header {
-  padding: 0;
-  height: 60px !important;
-  color: #fff;
-  text-align: center;
-  background: linear-gradient(135deg, #4c67ff, #5635df);
-}
-.el-col {
-  border-radius: 4px;
-  background: transparent;
-}
-.el-col-1 {
-  width: 5%;
-}
-.el-col-3 {
-  width: 25%;
-  float: right;
-}
-.el-col-6 {
-  width: 45%;
-}
-.bg-purple-dark {
-  background: #99a9bf;
-}
-.bg-purple {
-  background: none;
-  position: relative;
-  text-align: right;
-}
-.bg-purple span {
-}
-.bg-purple-light {
-  background: none;
-  height: 60px;
-  line-height: 60px;
-}
-.grid-content {
-  border-radius: 4px;
-  line-height: 60px;
-  height: 60px;
-}
-.row-bg {
-  position: relative;
-  background-color: none;
-}
-</style>
 
 <script>
+import subMenu from "@/components/subMenu";
 export default {
   data() {
     return {
@@ -173,5 +55,61 @@ export default {
       console.log(key, keyPath);
     },
   },
+  components: {
+    subMenu,
+  },
 };
 </script>
+<style>
+/* 顶栏布局 */
+.grid-content {
+  border-radius: 4px;
+  height: 60px;
+}
+.row-bg {
+  background: linear-gradient(135deg, #4c67ff, #5635df);
+}
+
+/* container样式 */
+.el-header {
+  color: #fff;
+  line-height: 60px;
+  background: linear-gradient(135deg, #4c67ff, #5635df);
+}
+.el-aside {
+  /* width: 64px !important; */
+}
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.bg-purple-light {
+  text-align: center;
+}
+.el-col {
+  width: 30%;
+}
+.el-col:nth-of-type(1) .grid-content {
+  text-align: left;
+}
+.el-col:nth-of-type(3) .grid-content {
+  text-align: right;
+}
+/* 顶部导航栏图片 */
+#logo-img {
+  display: inline-block;
+  background: #c0c4cc;
+  width: 35px;
+  height: 35px;
+  border-radius: 50%;
+  vertical-align: middle;
+  margin-right: 10px;
+}
+</style>
